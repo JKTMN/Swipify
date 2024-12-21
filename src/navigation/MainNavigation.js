590caf1efccdac87673397ed4e2a66d736f00c5d
Jamie <111710857+JKTMN@users.noreply.github.com>
2024-12-21 20:07:11 +0000

    Revert "Implemented New Components"
    This reverts commit 5348c237019e6eb43472de58533ac455857bd070.

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -3,16 +3,10 @@ import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import HomeScreen from '../screens/HomeScreen';
 import AccountScreen from '../screens/AccountScreen';
 import ThemeContext from '../context/ThemeContext';
-<<<<<<< Updated upstream
-=======
-import GameScreen from '../screens/GameScreen';
-import PlaylistScreen from '../screens/PlaylistsScreen';
->>>>>>> Stashed changes
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -37,7 +31,6 @@ function BottomTabNavigator() {
             },
          }}>
             <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarIcon: ({ color }) => <Ionicons name="home" size={30} color={color} />,headerTitleAlign: 'center'}}/>
-            <Tab.Screen name="Playlists" component={PlaylistScreen} options={{ tabBarIcon: ({ color }) => <MaterialCommunityIcons name="playlist-music-outline" size={30} color={color} />,headerTitleAlign: 'center'}}/>
             <Tab.Screen name="Account" component={AccountScreen} options={{ tabBarIcon: ({ color }) => <Ionicons name="person" size={30} color={color} />}}/>
         </Tab.Navigator>
     );
